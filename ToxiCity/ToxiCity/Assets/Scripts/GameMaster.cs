c8123f8e83aac6bf58bd7604b17e8b51030c3be1
PerenDarksbane <ajbimm@gmail.com>
2018-12-01 14:09:35 -0500

    Modified Gun
    Can shoot both directions
    Can swap between melee and ranged

---
+++ ToxiCity/ToxiCity/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameMaster : MonoBehaviour {
+	public Text InputText;
+	public static GameMaster Instance;
+	// Use this for initialization
+	void Awake()  {
+		if(Instance == null){
+			DontDestroyOnLoad(gameObject);
+			Instance = this;
+		}
+		else if (Instance != this){
+			Destroy (gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ ToxiCity/ToxiCity/Assets/Scripts/PlayerHurtboxTraits.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHurtboxTraits : MonoBehaviour {
+
+    public bool melee;
+    public float speed = 10;
+    public float xscale;
+	public float yscale;
+	public float damage;
+	public float knockback;
+	public string effect; 
+	public float time;
+	public Vector3 originPos;
+	public Vector3 displacePos;
+	public Vector3 scale;
+	public GameObject player;
+	public float combo = 0;
+	public float comboTime;
+
+	// Use this for initialization
+	void Start () {
+		originPos = this.transform.position;
+		player = GameObject.FindGameObjectWithTag("Player");
+		displacePos = originPos - player.transform.position;
+		scale.Set(xscale,yscale,1);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (melee)
+        {
+            this.transform.position = displacePos + player.transform.position;
+            this.transform.localScale = scale;
+        }
+        else
+        {
+            if(displacePos.x > 0)
+            {
+                this.transform.position = new Vector3(transform.position.x + 10, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                this.transform.position = new Vector3(transform.position.x - 10, transform.position.y, transform.position.z);
+            }
+            
+        }
+
+
+		if (time <= 0) {
+			Destroy(this.gameObject);
+		}
+		var trait = player.gameObject.GetComponent <PlayerTraits> ();
+		trait.combo += combo;
+		trait.comboTime = comboTime;
+	}
+
+	void FixedUpdate(){
+		time--;
+
+	}
+}
